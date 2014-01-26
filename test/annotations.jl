404008111eaa9da01dfa0780baf6dcdfe7e533a4
timholy <tim.holy@gmail.com>
2014-01-25 18:10:23 -0600

    Disable a window resize until updated in Gtk

--- test/annotations.jl
+++ test/annotations.jl
@@ -5,7 +5,8 @@ y = 8; x = 2;
 z[y,x] = 0
 zimg = convert(Image, z)
 imgc, img2 = ImageView.display(zimg,pixelspacing=[1,1]);
-Tk.set_size(ImageView.toplevel(imgc), 200, 200)
+# set_size(ImageView.toplevel(imgc), 200, 200)
+# Change to: resize!(ImageView.toplevel(imgc), 200, 200)
 idx = ImageView.annotate!(imgc, img2, ImageView.AnnotationText(x, y, "x", color=RGB(0,0,1)))
 # Also test the following:
 # ImageView.delete_annotation!(imgc, idx)
